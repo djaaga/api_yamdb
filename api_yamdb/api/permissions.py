57b5ff1ad505f93f3524d47f1322b8ed44e8d6f9
unknown <C:\Users\Vladimir\AppData\Roaming\The Bat!>
2023-01-10 16:51:22 +0700

    код завершен

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class ReviewPermission(permissions.BasePermission):
     """
-    Предоставление прав доступа на изменение 
+    Предоставление прав доступа на изменение
     отзывов и комментариев.
     """
 
@@ -12,16 +12,3 @@ class ReviewPermission(permissions.BasePermission):
             return True
 
         return obj.author == request.user
-
-
-# class IsAdminOrReadOnly(permissions.BasePermission):
-
-#     def has_permission(self, request, view):
-#         if request.method in permissions.SAFE_METHODS:
-#             return True
-#         if request.user.is_authenticated:
-#             return request.user.is_superuser or request.user.role in ('admin',)
-#         return False
-
-#     def has_object_permission(self, request, view, obj):
-#         return request.user.is_superuser or request.user.role in ('admin',)
