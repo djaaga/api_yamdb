b16e2f71659922db9c7bffa171e53ca803f6d8c9
Boris Gurev <djaaga5@gmail.com>
2023-01-10 21:00:17 +0400

    смержил ветку

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 
-from reviews.models import Category, Comment, Genre, Review, Title
+from reviews.models import Category, Genre, Title
 from users.models import User
 
 
@@ -117,41 +117,3 @@ class TitleSerializer(serializers.ModelSerializer):
         serializer = TitleGETSerializer(title)
         return serializer.data
 
-
-# class ReviewSerializer(serializers.ModelSerializer):
-#     """Сериализатор для модели Review."""
-# 
-#     author = serializers.StringRelatedField(
-#         read_only=True
-#     )
-# 
-#     class Meta:
-#         model = Review
-#         fields = (
-#             'id', 'text', 'author', 'score', 'pub_date')
-# 
-#     def validate(self, data):
-#         """Запрещает пользователям оставлять повторные отзывы."""
-#         if not self.context.get('request').method == 'POST':
-#             return data
-#         author = self.context.get('request').user
-#         title_id = self.context.get('view').kwargs.get('title_id')
-#         if Review.objects.filter(author=author, title=title_id).exists():
-#             raise serializers.ValidationError(
-#                 'Вы уже оставляли отзыв на это произведение'
-#             )
-#         return data
-# 
-# 
-# class CommentSerializer(serializers.ModelSerializer):
-#     """Сериализатор объектов класса Comment."""
-# 
-#     author = serializers.StringRelatedField(
-#         read_only=True
-#     )
-# 
-#     class Meta:
-#         model = Comment
-#         fields = (
-#             'id', 'text', 'author', 'pub_date')
-# 
